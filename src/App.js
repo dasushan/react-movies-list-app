085130bb623129191a5b603a922a3dc00529db8e
dasushan <53913896+dasushan@users.noreply.github.com>
2024-07-27 16:36:29 +0530

    added loading state

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
-
+  const [isLoading, setIsLoading] = useState(false);
   async function fetchMoviesHandler() {
+    setIsLoading(true);
     const response = await fetch('https://swapi.dev/api/films/');
     const data = await response.json();
 
@@ -18,7 +19,9 @@ function App() {
         releaseDate: movieData.release_date,
       };
     });
+
     setMovies(transformedMovies); 
+    setIsLoading(false);
      
       
   }
@@ -28,8 +31,13 @@ function App() {
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
+     
+        
+
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && <MoviesList movies={movies} />}
+        {isLoading && <p>Loading ...</p>}
+        
       </section>
     </React.Fragment>
   );
