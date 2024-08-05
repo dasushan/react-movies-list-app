dd216170ef7a8b5075d8976f4c47ebb2b0f9d5f5
dasushan <53913896+dasushan@users.noreply.github.com>
2024-08-05 11:47:37 +0530

    fetching on page load

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -29,11 +29,14 @@ function App() {
     };
   }, [error, clicked]);
 
-  async function fetchMoviesHandler() {
+  
+  
+
+  const  fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/film/');
+      const response = await fetch('https://swapi.dev/api/films/');
       if (!response.ok) {
         throw new Error('Something went wrong....Retrying');
       }
@@ -53,7 +56,11 @@ function App() {
       setError(error.message);
     }
     setIsLoading(false);
-  }
+  }, [])
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler])
 
   let content = <p>Found no movies</p>;
 
